b6e1b6e4b2db322bccbd914a91cfe4e3ff43dcdc
Begumgocer <103099104+Begumgocer@users.noreply.github.com>
2024-08-20 13:28:37 +0300

    quiz.py

--- quiz.py
+++ quiz.py
@@ -1,20 +1,19 @@
 #Python Flask ile yapılan basit bir quiz sitesi 
-#Gerekli kütüphanler
+#Gerekli kütüphaneler
+
 from flask import Flask, render_template, request, redirect, url_for
-import pypyodbc
+from flask_mysqldb import MySQL
 
-
-#Flask uygulamasının oluşturulması 
+#Flask uygulamasının oluşturulması
 app = Flask(__name__)
+
 #Veritabanı bağlantısı
-db = pypyodbc.connect(
-    'Driver={SQL Server};'
-    'Server=DESKTOP-FVBCG3G\SQLEXPRESS;'
-    'Database=ogrenciler;'
-    'Trusted_Connection=True;'
-)
-#
-dbase = db.cursor()
+app.config['MYSQL_HOST'] = 'localhost'  # MySQL sunucusunun adresi
+app.config['MYSQL_USER'] = 'root'  # MySQL kullanıcı adınız
+app.config['MYSQL_PASSWORD'] = 'begumgc'  # MySQL şifreniz
+app.config['MYSQL_DB'] = 'ogrenciler'  # Kullanmak istediğiniz veritabanı adı
+
+mysql = MySQL(app)
 
 #Test sorularının cevapları
 cevaplar = {
@@ -23,17 +22,17 @@ cevaplar = {
     'soru3': 'veri',
     'soru4': 'egitmek',
     'soru5': 'tekrar',
-    'soru6': 'deneyim'
-}
+    'soru6': 'deneyim'}
+
 #Link ile yönlendirme komutları
 @app.route('/')
 def ana():
     return render_template('anasayfa.html')
 
-#Kullanıcının sorular sayfasında bulunan form kısımına verdiği cevapların alınması 
+#Kullanıcının sorular sayfasında bulunan form kısımına verdiği cevapların alınması
 @app.route('/sorular', methods=['GET', 'POST'])
 def sorular():
-#Formda boş bir değer olduğunda; boş değerin hangi kısımdan geldiğinin tespit edilmesi ve uygun hata mesajının ekrana yazdırılması
+    #Formda boş bir değer olduğunda; boş değerin hangi kısımdan geldiğinin(ad soyad veya test soruları) tespit edilmesi ve uygun hata mesajının ekrana yazdırılması
     hata_mesaji = None
     if request.method == 'POST':
         ad = request.form.get('ad')
@@ -53,7 +52,7 @@ def sorular():
             hata_mesaji = "Lütfen tüm soruları cevaplayınız!"
             return render_template('sonuc.html', hata_mesaji=hata_mesaji)
 
-#Verilen doğru cevaplara göre test skorunun yüzelik olarak hesaplanması 
+        #Verilen doğru cevaplara göre test skorunun yüzdelik olarak hesaplanması
         soru_sayisi = len(cevaplar)
         puan = 0
         for soru, dogru_cevap in cevaplar.items():
@@ -61,10 +60,12 @@ def sorular():
                 puan += 1
 
         yuzdelik_puan = (puan / soru_sayisi) * 100
-#Kulanıcının cevaplarının veritabanına kaydedilmesi
-        dbase.execute("""
+
+        #Kullanıcının cevaplarının veritabanına kaydedilmesi
+        cur = mysql.connection.cursor()
+        cur.execute("""
             INSERT INTO OgrenciCevaplari (ad, soyad, yas, soru1, soru2, soru3, soru4, soru5, soru6, puan)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
         """, (
             ad, soyad, yas,
             request.form.get('soru1'),
@@ -75,8 +76,10 @@ def sorular():
             request.form.get('soru6'),
             yuzdelik_puan
         ))
-        db.commit()
-#Alınan puanın sorular sayfasında ekrana yazdılırması
+        mysql.connection.commit()
+        cur.close()
+
+        #Alınan puanın sorular sayfasında ekrana yazdırılması
         return redirect(url_for('sonuc', puan=yuzdelik_puan))
     return render_template('sorular.html')
 
